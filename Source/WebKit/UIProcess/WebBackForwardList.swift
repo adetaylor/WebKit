b2b97ccb8e2e9c17aeb52d15d74a5b62eb3bc8ea
Adrian Taylor <adrian_taylor@apple.com>
2025-05-02 12:10:01 +0100

    Attempting to pass frame state

--- Source/WebKit/UIProcess/WebBackForwardList.swift
+++ Source/WebKit/UIProcess/WebBackForwardList.swift
@@ -6,8 +6,20 @@ public func WebBackForwardList_swiftBit_thunk(webBackForwardList: WebKit.WebBack
     return webBackForwardList.swiftBit()
 }
 
+
+@available(WK_IOS_TBA, WK_MAC_TBA, WK_XROS_TBA, *)
+@_expose(Cxx)
+public func WebBackForwardList_swiftAddItem_thunk(webBackForwardList: WebKit.WebBackForwardList, navigatedFrameState: WebKit.FrameState) {
+    return webBackForwardList.swiftAddItem(navigatedFrameState: navigatedFrameState)
+}
+
+
 extension WebKit.WebBackForwardList {
     func swiftBit() {
         print("Hello!");
+    }
+
+    func swiftAddItem(navigatedFrameState: WebKit.FrameState) {
+        print("Got frame state")
     }
 }
