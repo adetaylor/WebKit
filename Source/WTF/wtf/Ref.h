98aa3ae00d848702d179b82e8dd32edf54b12560
Adrian Taylor <adrian_taylor@apple.com>
2025-10-02 12:06:38 +0100

    ref incude swift/bridging

--- Source/WTF/wtf/Ref.h
+++ Source/WTF/wtf/Ref.h
@@ -33,6 +33,10 @@
 #include <wtf/StdLibExtras.h>
 #include <wtf/TypeCasts.h>
 
+#ifdef __swift__
+#include <swift/bridging>
+#endif
+
 #if ASAN_ENABLED
 extern "C" void __asan_poison_memory_region(void const volatile *addr, size_t size);
 extern "C" void __asan_unpoison_memory_region(void const volatile *addr, size_t size);
