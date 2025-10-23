bb98d8a9ea46427556f05f9ae271013dfdb08b76
Adrian Taylor <adrian_taylor@apple.com>
2025-10-23 19:10:31 +0100

    The only nullability required

--- Source/WebKit/UIProcess/AuxiliaryProcessProxy.h
+++ Source/WebKit/UIProcess/AuxiliaryProcessProxy.h
@@ -153,7 +153,7 @@ public:
     {
         return m_connection == &connection;
     }
-    static AuxiliaryProcessProxy* fromConnection(const IPC::Connection&);
+    static AuxiliaryProcessProxy* _Nonnull fromConnection(const IPC::Connection&);
 
     void addMessageReceiver(IPC::ReceiverName, IPC::MessageReceiver&);
     void addMessageReceiver(IPC::ReceiverName, uint64_t destinationID, IPC::MessageReceiver&);
