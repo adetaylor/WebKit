6060c52e018ace476bf823130cb61d56d98a384f
Adrian Taylor <adrian_taylor@apple.com>
2025-12-08 12:32:55 +0000

    Ref SWIFT_RETURNS_INDEPENDENT_VALUE

--- Source/WTF/wtf/Ref.h
+++ Source/WTF/wtf/Ref.h
@@ -134,7 +134,7 @@ public:
     T* ptrAllowingHashTableEmptyValue() LIFETIME_BOUND { ASSERT(m_ptr || isHashTableEmptyValue()); return PtrTraits::unwrap(m_ptr); }
 
     T* operator->() const LIFETIME_BOUND { ASSERT(m_ptr); return PtrTraits::unwrap(m_ptr); }
-    T* ptr() const LIFETIME_BOUND RETURNS_NONNULL { ASSERT(m_ptr); return PtrTraits::unwrap(m_ptr); }
+    T* ptr() const LIFETIME_BOUND RETURNS_NONNULL SWIFT_RETURNS_INDEPENDENT_VALUE { ASSERT(m_ptr); return PtrTraits::unwrap(m_ptr); }
     T* unsafePtr() const RETURNS_NONNULL { ASSERT(m_ptr); return PtrTraits::unwrap(m_ptr); } // FIXME: Replace with ptr() then remove.
     T& get() const LIFETIME_BOUND { ASSERT(m_ptr); return *PtrTraits::unwrap(m_ptr); }
     T& unsafeGet() const { ASSERT(m_ptr); return *PtrTraits::unwrap(m_ptr); } // FIXME: Replace with get() then remove.
