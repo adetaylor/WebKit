aa8b8e004c3f6b7f5b6d8cdc5c3b8ba671180600
Adrian Taylor <adrian_taylor@apple.com>
2026-01-26 09:01:19 +0000

    Swift WebBackForwardList (off by default) - C++ call tweaks https://bugs.webkit.org/show_bug.cgi?id=305483 rdar://159589815
    Reviewed by NOBODY (OOPS!).
    
    We're introducing a Swift version of the Back Forward List. For the general
    design and rationale, see
    https://github.com/WebKit/WebKit/pull/55393
    It will be landed as a series of separate commits in order to ease code review.
    
    In this commit we change various bits of C++ code which call methods on
    WebBackForwardList, to be aware of slightly different calling conventions
    when calling APIs on the Swift version.
    
    Specifically, we took a decision NOT to allow Swift types to be kept in
    Ref<T> and RefPtr<T>, because it's redundant - the Swift type is itself
    reference counted. This could have been achieved using specialized traits,
    but was felt to be confusing, and that it was better to add various #ifdefs
    instead - that's what this commit does.
    
    A few specific notes:
    * WTF::Functions and WTF::CompletionHandlers need to be explicitly packaged
      up to pass them into Swift; this is because we can only own them as
      reference-counted types in Swift.
    * Often we need to use . with Swift types instead of -> with Ref.
    * Instead of a RefPtr, we store a std::optional of the Swift type.

--- Source/WebKit/UIProcess/ViewGestureController.h
+++ Source/WebKit/UIProcess/ViewGestureController.h
@@ -97,7 +97,11 @@ namespace WebKit {
 
 class ViewSnapshot;
 class WebBackForwardList;
+#if ENABLE(BACK_FORWARD_LIST_SWIFT)
+class WebBackForwardListWrapper;
+#else
 using WebBackForwardListWrapper = WebBackForwardList;
+#endif
 class WebBackForwardListItem;
 class WebPageProxy;
 class WebProcessProxy;
@@ -233,7 +237,11 @@ private:
     void didStartProvisionalOrSameDocumentLoadForMainFrame();
 
 #if PLATFORM(COCOA)
+#if ENABLE(BACK_FORWARD_LIST_SWIFT)
+    std::optional<WebBackForwardList> backForwardListForNavigation() const;
+#else
     WebBackForwardList* backForwardListForNavigation() const;
+#endif
 #endif
 
     class SnapshotRemovalTracker : public CanMakeCheckedPtr<SnapshotRemovalTracker> {
